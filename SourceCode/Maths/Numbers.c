374eb7caecd7215af6d24f949b9d99379dbd1636
Amarjit Singh <amarjitmca04@gmail.com>
2020-04-30 16:02:30 +0530

    Bug 1324394: Allow origins to be edited in the Password Manager 	Resolution: 		Divide By Zero was cause 		Fixed it in Function div

--- SourceCode/Maths/Numbers.c
+++ SourceCode/Maths/Numbers.c
@@ -14,3 +14,10 @@ int mul(int x, int y) {
   return x * y;
 }
 
+// Following Function Added In Testing Branch
+int div(int x, int y) {
+	if( y != 0 )
+ 		 return x / y;
+	else 
+		printf("Divide By Zero...");
+}
