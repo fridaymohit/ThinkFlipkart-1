3a460e78bdeb0ddad822952aa6e06ef988b91a31
techhue <amarjitmca04@gmail.com>
2020-04-30 12:56:33 +0530

    Update String.cpp
    Added To Upper Functionality In String Module
    Added upper(char []) function in String.cpp

--- SourceCode/String/String.cpp
+++ SourceCode/String/String.cpp
@@ -4,6 +4,10 @@ void greetings( char hello[]) {
 	printf("\n%s", hello);
 }
 
+void toUpper( char string[] ) {
+	printf("Making String UPPER...");	
+}
+
 int main() {
 	char greet[] = "Hello World!";
 
